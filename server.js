63946d0d9b118a7f63c86f6043fabff200e8f5b1
Rupesh Ojha <bhs.rupesh@gmail.com>
2021-11-06 22:57:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ app.get('/', (req, res) => {
   res.sendFile('public/index.html', { root: '.' });
 });
 
-app.listen(5000, console.log('Server running at port 5000.'));
+app.listen(process.env.PORT, console.log('Server running at port 5000.'));
